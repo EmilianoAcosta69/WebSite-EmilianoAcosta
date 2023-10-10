5cd2d684d9af15d9d7b2bb5046f4a581764d1d25
Emiliano Acosta <emilianoacosta18@outlook.com>
2023-10-10 16:04:16 -0300

    se agrega formulario de contacto funcional a la pagina

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#formContacto');
+const buttonMail = document.querySelector('#mailSend');
+
+form.addEventListener('submit',handleSubmit)
+
+function handleSubmit(event){
+    event.preventDefault();
+    const formulario = new FormData(this)
+    buttonMail.setAttribute('href',`mailto:emilianoac14@gmail.com?subject=
+    ${formulario.get('name')} ${formulario.get('email')}
+    &body=${formulario.get('message')}`)
+    buttonMail.click()
+}
